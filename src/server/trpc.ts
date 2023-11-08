3bea2c185f02d2184609f46b1066913c7c60fe5b
quang <nguyenthequang080203@gmail.com>
2023-11-08 11:10:43 +0700

    create trpc router

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -2,5 +2,7 @@ import { initTRPC } from "@trpc/server";
 
 const t = initTRPC.create();
 
-export const router = t.router;
-export const publicProcedure = t.procedure;
+export const router = t.router; //Express.js router provided by tRPC. It's used to define and handle your API routes.
+export const publicProcedure = t.procedure; //called by client to execute specific tasks on the server.
+
+//we will  have routers that have procedures in them.
