635b2b69011ca07799feaec2264d24db7ed87678
quang <nguyenthequang080203@gmail.com>
2023-11-08 16:48:57 +0700

    add todo feature with addTodo mutation

--- src/app/_components/TodoList.tsx
+++ src/app/_components/TodoList.tsx
@@ -1,12 +1,39 @@
 'use client'
 
+import { useState } from "react";
 import { trpc } from "../_trpc/client"
 
 function TodoList() {
   const getTodos = trpc.getTodos.useQuery();
+  const addTodo = trpc.addTodo.useMutation({
+    onSettled: () => {
+      getTodos.refetch() //when addTodo is setteled, we refetch getTodos
+    }
+  });
+
+  const [content, setContent] = useState("");
 
   return (
-    <div>{JSON.stringify(getTodos.data)}</div>
+    <div>
+      <div className="text-white my-5 text-3xl">{JSON.stringify(getTodos.data)}</div>
+      <div className="flex gap-3 items-center">
+        <label htmlFor="content">Content</label>
+        <input
+          id="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+           className="flex-grow text-black bg-white rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-4 py-2"
+        />
+        <button onClick={async () => {
+          if (content.length) {
+            addTodo.mutate(content);
+            setContent("")
+          }
+        }}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        >Add Todo</button>
+      </div>
+    </div>
   )
 }
 
