4170fdc7a030604109c5e872a3e150be09b21529
quang <nguyenthequang080203@gmail.com>
2023-11-08 16:42:41 +0700

    add addTodo procedure

--- src/app/_components/TodoList.tsx
+++ src/app/_components/TodoList.tsx
@@ -4,6 +4,7 @@ import { trpc } from "../_trpc/client"
 
 function TodoList() {
   const getTodos = trpc.getTodos.useQuery();
+
   return (
     <div>{JSON.stringify(getTodos.data)}</div>
   )
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/better-sqlite3"
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import Database from "better-sqlite3";
+import { z } from "zod"
 
 import { publicProcedure, router } from "./trpc";
 
@@ -14,6 +15,10 @@ migrate(db, {migrationsFolder: 'drizzle'})
 export const appRouter = router({
     getTodos: publicProcedure.query(async () => { //.query() is used for fetching data from the server
         return await db.select().from(todos).all();
+    }),
+    addTodo: publicProcedure.input(z.string()).mutation(async (opts) => {
+        await db.insert(todos).values({content: opts.input, done: 0}).run();
+        return true;
     })
 })
 
