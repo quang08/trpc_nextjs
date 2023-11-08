06d4d921b6a2db1653fba747235ef7e9bf90ede1
quang <nguyenthequang080203@gmail.com>
2023-11-08 16:32:26 +0700

    query db with drizzle orm

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,19 @@
+import { drizzle } from "drizzle-orm/better-sqlite3"
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+import Database from "better-sqlite3";
+
 import { publicProcedure, router } from "./trpc";
+
+import { todos } from "@/app/db/schema";
+
+const sqlite = Database("sqlite.db")
+const db = drizzle(sqlite)
+
+migrate(db, {migrationsFolder: 'drizzle'})
 
 export const appRouter = router({
     getTodos: publicProcedure.query(async () => { //.query() is used for fetching data from the server
-        return [10, 20, 30]
+        return await db.select().from(todos).all();
     })
 })
 
